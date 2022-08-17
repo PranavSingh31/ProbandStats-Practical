b13f25c495fe116ad3b96ff2fb3a0dd11b9db119
Pranav <prsingh1347@gmail.com>
2022-08-17 23:05:28 +0530

    Exp2

--- Exp-1/1x2.R
+++ Exp-1/1x2.R
@@ -1,6 +1,6 @@
-num <- as.integer(readline(prompt="Enter a number: "))
+num = as.integer(readline(prompt="Enter a number: "))
 factorial = 1
-if(num < 0){
+if(num < 0) {
   print("Sorry, factorial does not exist for negative numbers")
 } else if(num == 0) {
   print("The factorial of 0 is 1")
---
+++ Exp-2/2x1a.R
@@ -0,0 +1,3 @@
+data<-rep(c('G','S','B'),times=c(20,30,50))
+sample(data,size=10)
+
