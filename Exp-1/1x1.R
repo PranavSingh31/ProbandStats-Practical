05e85c34b6d741d0b53bae9cd08f32da77f17289
Pranav <prsingh1347@gmail.com>
2022-11-09 17:35:30 +0530

    comments added

--- Exp-1/1x1.R
+++ Exp-1/1x1.R
@@ -1,4 +1,6 @@
-c<-seq(5,30,by=5)
-cat(c)
-max(c)
-min(c)
+c<-seq(5,30,by=5) 
+#creating vector 5,10,15,20,25,30 
+#using sequence function (5-initial point, 30-final point, by=5 is gap)
+cat(c) #printing the vector
+max(c) #printing maximum element of the vector
+min(c) #printing minimum element of the vector
